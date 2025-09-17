17b058363a97b46dcac6e66a8b0206f0d40c1e91
krisha0803 <krishas089@gmail.com>
2025-09-17 14:39:19 +0530

    Update Cat.js

--- Cat.js
+++ Cat.js
@@ -1,12 +1,16 @@
-class Cat extends Animal{
-  overwrite parent constuctor
-  constructor(){
-    //create a clone of parent 
-  super("Cat", name, "mewww"); //calls parent creation
+class Cat extends Animal {
+  //overwrite parent constructor
+  constructor(name, language, mustashStyle="curly") {
+    // create a clone of parent
+    super("Cat", name, language="mewwwww"); //calls parent creation
     
-    //overwrite parent function 
- talk(){
-   print(`hellooo, meooww. i am a ${this.breed}!`)
-    }
+    // personal class property
+    this.mustashStyle = mustashStyle
+  }
+
+  //overwrite parent function
+  talk() {
+    // Another way of String concotenation
+    print(`I'm a ${this.breed}, with ${this.mustashStyle} mustash my name is ${this.name}...${this.language}`);
   }
 }
